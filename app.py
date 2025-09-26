16829b4b9cb0bfabea690efa467ec3ecbf4099af
unknown <carlos.amigodoespiritosanto@gmail.com>
2025-09-26 20:02:57 -0300

    atualizando arquivos

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def servico_detalhe(servico_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
